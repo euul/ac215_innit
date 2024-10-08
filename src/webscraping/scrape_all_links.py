fd72c64772b43083237ffa87022416613dbeead5
James Cao <jamescao@fas.harvard.edu>
2024-10-08 17:26:16 -0400

    updated Dockerfile and added scraping code

--- src/webscraping/scrape_all_links.py
+++ src/webscraping/scrape_all_links.py
@@ -3,14 +3,20 @@ from bs4 import BeautifulSoup
 import pandas as pd
 
 # Function to scrape titles and links from a given URL
-def scrape_page(url):
-    full_url = "https://learnenglish.britishcouncil.org/" + url
-
-    # Headers to include in the request
-    headers = {
+def scrape_page(url, teens):
+    if teens == False:
+        full_url = "https://learnenglish.britishcouncil.org/" + url
+        headers = {
         "authority": "learnenglish.britishcouncil.org",
         "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36"
     }
+    else:
+        full_url = "https://learnenglishteens.britishcouncil.org/" + url
+        headers = {
+        "authority": "learnenglishteens.britishcouncil.org",
+        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36"
+    }
+
 
     # Make the GET request
     response = requests.get(full_url, headers=headers)
@@ -20,7 +26,10 @@ def scrape_page(url):
         soup = BeautifulSoup(response.text, 'html.parser')
 
         # Find all elements with the specific class
-        title_elements = soup.find_all('div', class_='field field--name-node-title field--type-ds field--label-hidden field--item')
+        if teens == False: 
+            title_elements = soup.find_all('div', class_='field field--name-node-title field--type-ds field--label-hidden field--item')
+        else: 
+            title_elements = soup.find_all('div', class_='content-landing-list-content')
 
         # Extract title and link for each element
         titles = []
@@ -36,27 +45,37 @@ def scrape_page(url):
         print(f"Failed to retrieve the page: {url}. Status code: {response.status_code}")
         return [], []  # Return empty lists if the request fails
 
-# Read URLs from a text file (each URL on a new line)
-with open('target_links.txt', 'r') as f:  # Replace with the actual name of your text file
-    urls = [line.strip() for line in f.readlines()]
 
-# Initialize lists to store all the scraped data
-all_titles = []
-all_links = []
+def scrape_links(target_links,teens):
+    # Read URLs from a text file (each URL on a new line)
+    with open(target_links, 'r') as f:  # Replace with the actual name of your text file
+        urls = [line.strip() for line in f.readlines()]
 
-# Loop through each URL in the text file and scrape the titles/links
-for url in urls:
-    print(f"Scraping: {url}")
-    titles, links = scrape_page(url)
+    # Initialize lists to store all the scraped data
+    all_titles = []
+    all_links = []
+
+    # Loop through each URL in the text file and scrape the titles/links
+    for url in urls:
+        print(f"Scraping: {url}")
+        titles, links = scrape_page(url, teens)
+        
+        # Append the scraped titles and links to the main lists
+        all_titles.extend(titles)
+        all_links.extend(links)
+
+    # Save the titles and links into a pandas DataFrame
+    df = pd.DataFrame({'Title': all_titles, 'Link': all_links})
+
+    # Save the DataFrame to a CSV file
+    if teens == False:
+        df.to_csv('scraped_all_links.csv', index=False)
+        print("Scraping complete. All data saved to 'scraped_all_links.csv'.")
+    else:
+        df.to_csv('scraped_all_links_teens.csv', index=False)
+        print("Scraping complete. All data saved to 'scraped_all_links_teens.csv'.")
+
     
-    # Append the scraped titles and links to the main lists
-    all_titles.extend(titles)
-    all_links.extend(links)
 
-# Save the titles and links into a pandas DataFrame
-df = pd.DataFrame({'Title': all_titles, 'Link': all_links})
-
-# Save the DataFrame to a CSV file
-df.to_csv('scraped_all_links.csv', index=False)
-
-print("Scraping complete. All data saved to 'scraped_listening_links.csv'.")
+scrape_links("target_links.txt",False)
+scrape_links("target_links_teens.txt",True)
--- src/webscraping/scrape_all_transcripts.py
+++ src/webscraping/scrape_all_transcripts.py
@@ -2,95 +2,125 @@ import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
-# Read all the links from the CSV file
-df = pd.read_csv('scraped_all_links.csv')
+def scrape_transcripts(csv_name, teens= False):
+    # Read all the links from the CSV file
+    df = pd.read_csv(csv_name)
 
-# Initialize lists to store the scraped data
-transcripts = []
-levels = []
-topics = []
-links_processed = []
-missing_transcripts_urls = []
+    # Initialize lists to store the scraped data
+    transcripts = []
+    levels = []
+    topics = []
+    links_processed = []
+    missing_transcripts_urls = []
 
-# Loop through each link in the CSV
-for index, row in df.iterrows():
-    # Construct the full link
-    link_complete = 'https://learnenglish.britishcouncil.org' + row['Link']
-    print(f"Scraping {link_complete}...")
-
-    # Headers to include in the request
-    headers = {
-        "authority": "learnenglish.britishcouncil.org",
-        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36"
-    }
-
-    # Make the request
-    response = requests.get(link_complete, headers=headers)
-
-    if response.status_code == 200:
-        # Parse the HTML
-        soup = BeautifulSoup(response.text, 'html.parser')
-
-        # Transcript or Reading Text
-        transcript_div = soup.find("div", class_="field field--name-field-tapescript field--type-text-long field--label-hidden field--item")
-        if not transcript_div:
-            # Check for 'Reading text' if 'Transcript' is not found
-            transcript_div = soup.find("div", class_="field field--name-field-reading-text field--type-text-long field--label-hidden field--item")
-        
-        if transcript_div:
-            transcript = transcript_div.get_text(separator='\n')
-        else:
-            transcript = "Transcript/Reading text not found"
-            # Add the URL to the missing list if neither is found
-            missing_transcripts_urls.append(link_complete)
-
-        # Label (Language Level)
-        level_div = soup.find('h3', string="Language level")
-        if level_div:
-            level_div = level_div.find_next_sibling('div')
-            level = level_div.find("div", class_="field--item").find("a").get_text() if level_div else "Level not found"
-        else:
-            level = "Level not found"
-
-        # Topic (Handling multiple topics)
-        topic_div = soup.find('h3', string="Topics")
-        if topic_div:
-            topic_div = topic_div.find_next_sibling('div')
-            topic_items = topic_div.find_all("div", class_="field--item")
-            # Collect all topics and concatenate them
-            topic = ', '.join([item.get_text() for item in topic_items]) if topic_items else "Topic not found"
-        else:
-            topic = "Topic not found"
-
-        # Append the data to the lists
-        transcripts.append(transcript)
-        levels.append(level)
-        topics.append(topic)
-        links_processed.append(link_complete)
-
+    if teens == False:
+        url_root = 'https://learnenglish.britishcouncil.org'
+        # Headers to include in the request
+        headers = {
+            "authority": "learnenglish.britishcouncil.org",
+            "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36"
+        }
     else:
-        print(f"Failed to retrieve {link_complete}. Status code: {response.status_code}")
-        transcripts.append("Failed to retrieve")
-        levels.append("Failed to retrieve")
-        topics.append("Failed to retrieve")
-        links_processed.append(link_complete)
+        url_root = 'https://learnenglishteens.britishcouncil.org'
+        # Headers to include in the request
+        headers = {
+            "authority": "learnenglishteens.britishcouncil.org",
+            "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36"
+        }
+    # Loop through each link in the CSV
+    for index, row in df.iterrows():
+        # Construct the full link
+        link_complete = url_root + row['Link']
+        print(f"Scraping {link_complete}...")
 
-# Create a DataFrame with all the scraped data
-scraped_data = pd.DataFrame({
-    'Link': links_processed,
-    'Transcript': transcripts,
-    'Label': levels,
-    'Topic': topics
-})
+        # Make the request
+        response = requests.get(link_complete, headers=headers)
 
-# Save the DataFrame to a CSV file
-scraped_data.to_csv('scraped_all_content.csv', index=False)
-print(f"All data has been written to scraped_all_content.csv")
+        if response.status_code == 200:
+            # Parse the HTML
+            soup = BeautifulSoup(response.text, 'html.parser')
 
-# Save the URLs of pages missing transcript/reading text to a txt file
-with open('missing_transcripts.txt', 'w') as f:
-    for url in missing_transcripts_urls:
-        f.write(f"{url}\n")
+            # Transcript or Reading Text
+            cases = [
+                {"tag": "div", "class": "field field--name-field-tapescript field--type-text-long field--label-hidden field--item"},
+                {"tag": "div", "class": "field field--name-field-reading-text field--type-text-long field--label-hidden field--item"},
+                {"tag": "div", "class": "field field--name-field-transcript field--type-text-long field--label-hidden field--item"}
+                ]
+            
+            for case in cases:
+                tag = case.get("tag")
+                class_name = case.get("class")
+            transcript_div = soup.find(tag, class_=class_name)
 
-print(f"Missing transcripts or reading texts URLs have been saved to missing_transcripts.txt")
+            if transcript_div:
+                transcript = transcript_div.get_text(separator='\n')
+            else:
+                transcript = "Transcript/Reading text not found"
+                # Add the URL to the missing list if neither is found
+                missing_transcripts_urls.append(link_complete)
 
+            # Label (Language Level)
+            if teens == False: 
+                level_div = soup.find('h3', string="Language level")
+            else: 
+                level_div = soup.find('div', string="Language level")
+
+            if level_div:
+                level_div = level_div.find_next_sibling('div')
+                level = level_div.find("div", class_="field--item").find("a").get_text() if level_div else "Level not found"
+            else:
+                level = "Level not found"
+            
+
+            # Topic (Handling multiple topics)
+            if teens == False: 
+                topic_div = soup.find('h3', string="Topics")
+            else: 
+                topic_div = soup.find('div', string="Topics")
+
+            if topic_div:
+                topic_div = topic_div.find_next_sibling('div')
+                topic_items = topic_div.find_all("div", class_="field--item")
+                # Collect all topics and concatenate them
+                topic = ', '.join([item.get_text() for item in topic_items]) if topic_items else "Topic not found"
+            else:
+                topic = "Topic not found"
+
+            # Append the data to the lists
+            transcripts.append(transcript)
+            levels.append(level)
+            topics.append(topic)
+            links_processed.append(link_complete)
+
+        else:
+            print(f"Failed to retrieve {link_complete}. Status code: {response.status_code}")
+            transcripts.append("Failed to retrieve")
+            levels.append("Failed to retrieve")
+            topics.append("Failed to retrieve")
+            links_processed.append(link_complete)
+
+    # Create a DataFrame with all the scraped data
+    scraped_data = pd.DataFrame({
+        'Link': links_processed,
+        'Transcript': transcripts,
+        'Label': levels,
+        'Topic': topics
+    })
+
+    # Save the DataFrame to a CSV file
+    if teens == False:
+        scraped_data.to_csv('scraped_all_content.csv', index=False)
+        print(f"All data has been written to scraped_all_content.csv")
+    else:
+        scraped_data.to_csv('scraped_all_content_teens.csv', index=False)
+        print(f"All data has been written to scraped_all_content_teens.csv")
+
+    # Save the URLs of pages missing transcript/reading text to a txt file
+    with open('missing_transcripts.txt', 'w') as f:
+        for url in missing_transcripts_urls:
+            f.write(f"{url}\n")
+
+    print(f"Missing transcripts or reading texts URLs have been saved to missing_transcripts.txt")
+
+#scrape_transcripts("scraped_all_links.csv",False)
+scrape_transcripts("scraped_all_links_teens.csv",True)
