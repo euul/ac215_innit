8a3a56ab9df440ed504868514de9d2ecde1242c1
euul <jcaocowDX@gmail.com>
2024-12-07 21:11:20 -0500

    /deploy-app: trying with an entrypoint?

--- src/deployment/docker-entrypoint.sh
+++ src/deployment/docker-entrypoint.sh
@@ -4,11 +4,19 @@ echo "Container is running!!!"
 
 # Authenticate gcloud using service account
 gcloud auth activate-service-account --key-file $GOOGLE_APPLICATION_CREDENTIALS
-
-# Set GCP Project Details (add --quiet to skip confirmation)
-gcloud config set project $GCP_PROJECT --quiet
-
+# Set GCP Project Details
+gcloud config set project $GCP_PROJECT
 # Configure GCR
 gcloud auth configure-docker gcr.io -q
 
-/bin/bash
+args="$@"
+echo $args
+
+if [[ -z ${args} ]]; 
+then
+    #/bin/bash
+    pipenv shell
+else
+  #/bin/bash $args
+  pipenv run $args
+fi
